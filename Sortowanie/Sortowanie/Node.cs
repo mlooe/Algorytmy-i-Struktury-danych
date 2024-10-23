c99258b7830c533de989be9666ee5252dcc84807
Wiktor Krasiński <147048568+mlooe@users.noreply.github.com>
2024-10-23 09:29:24 +0200

    Add files via upload

---
+++ Sortowanie/Sortowanie/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sortowanie
+{
+    public class Node
+    {
+        public Node next;
+        public Node prev;
+        public int data;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
+
+        // Drzewo vvv
+
+
+
+
+        public class NodeT
+        {
+            public NodeT lewe;
+            public NodeT prawe;
+            public NodeT rodzic;
+            public int data;
+        }
+
+    }
+}
